5c3840008c2f33c8c2e59cd4936dd2d3abcd9b17
Vanura <Efimovv2003@yandex.ru>
2023-04-02 18:00:35 +0700

    ДЗ 3

---
+++ lesson 3/hwl3ex21/Program.cs
@@ -0,0 +1,12 @@
+﻿double[] cords = new double[6];
+            string[] cordsnames = new string[] { "x1", "y1", "z1", "x2", "y2", "z2" };
+            // Получиться таблица координат строки = точки
+            Console.WriteLine("Введите:");
+            for (int i = 0; i<6; i++) 
+            {
+                Console.Write(cordsnames[i] + ":");
+                cords[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double distance = Math.Sqrt(Math.Pow(cords[3] - cords[0], 2) + Math.Pow(cords[4] - cords[1], 2) + Math.Pow(cords[5] - cords[2], 2));
+            Console.WriteLine(distance);
---
+++ lesson 3/hwl3ex23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i=1;i<=n;i++)
+{
+int k = i*i*i;
+Console.Write(k);
+}
