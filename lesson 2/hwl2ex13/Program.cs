0664d0763278204703c8abc8a354037210ec8d60
Vanura <Efimovv2003@yandex.ru>
2023-04-02 17:32:52 +0700

    ДЗ 2

---
+++ lesson 2/hwl2ex13/Program.cs
@@ -0,0 +1,7 @@
+﻿string a = Convert.ToString(Convert.ToInt32(Console.ReadLine()));
+//Почему-то VS Code, в отличие от обычного VS не хочет, чтобы я записывал сразу в переменную, но такой костыль помог
+// Program.cs(1,12): warning CS8600: Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+//Program.cs(2,19): warning CS8602: Разыменование вероятной пустой ссылки.
+//Не понимаю в чем проблема
+if (a.Length < 3) {Console.WriteLine("Нет третьей цифры");}
+else {Console.WriteLine(a[2]);};
---
+++ lesson 2/hwl2ex15/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+            switch ( a )
+            {
+                case 1:
+                    Console.WriteLine("Нет");
+                    break;
+                case 2:
+                    Console.WriteLine("Нет");
+                    break;
+                case 3:
+                    Console.WriteLine("Нет");
+                    break;
+                case 4:
+                    Console.WriteLine("Нет");
+                    break;
+                case 5:
+                    Console.WriteLine("Нет");
+                    break;
+                case 6:
+                    Console.WriteLine("Да");
+                    break;
+                case 7:
+                    Console.WriteLine("Да");
+                    break;
+                default:
+                    Console.WriteLine("Это не день недели");
+                    break;
+
+            }
